9dff08c829706c4bdfc0753b6b0270f76002ff49
renatodellosso <rddellosso4@gmail.com>
2024-11-09 17:28:46 -0500

    Enhance layout spacing, adjust NavBar padding, and add new blog post on RMUD2 game design analysis

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,8 +4,8 @@ export default function Layout({
 	children: React.ReactNode;
 }) {
 	return (
-		<main className="flex justify-center">
-			<div className="flex flex-col justify-center w-1/2">{children}</div>
+		<main className="flex justify-center mt-4 mb-16">
+			<div className="flex flex-col justify-center w-1/2 gap-4">{children}</div>
 		</main>
 	);
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <div className="border-b-[1px] border-white flex flex-row gap-4">
+        <div className="border-b-[1px] border-white flex flex-row gap-4 pl-1">
             <Link className="text-xl text-primary hover:text-accent transition-colors decoration-transparent hover:decoration-transparent" href="/">
                 Renato
             </Link>
